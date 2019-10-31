27297e713d55a67443f13bd9f9b9af8cb991b654
TomasS <43637908+GrimReaper6667@users.noreply.github.com>
2019-10-31 11:42:44 +0100

    cleanup

---
+++ src/components/pages/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export class Home extends Component {
+    render() {
+        return (
+            <section>
+                <h1>
+                    Welkom bij het NMVW
+                </h1>
+            </section>
+        )
+    }
+}
+
+export default Home
