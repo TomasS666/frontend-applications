4fe924a09a545d125c7774b5f86d33d2879104d2
TomasS <43637908+GrimReaper6667@users.noreply.github.com>
2019-11-01 13:44:17 +0100

    image of concept

---
+++ build/precache-manifest.41203aca296ca676df84d773d273e29e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94ff0eabbbc8b27ebbab0bc40513622b",
+    "url": "/frontend-applications/index.html"
+  },
+  {
+    "revision": "5d3fd1e9d1082a553760",
+    "url": "/frontend-applications/static/css/main.c1b06a08.chunk.css"
+  },
+  {
+    "revision": "53d46a7257fc7cd927fa",
+    "url": "/frontend-applications/static/js/2.a2b35eec.chunk.js"
+  },
+  {
+    "revision": "5d3fd1e9d1082a553760",
+    "url": "/frontend-applications/static/js/main.f4011707.chunk.js"
+  },
+  {
+    "revision": "ca4a6cd35c766fc05f37",
+    "url": "/frontend-applications/static/js/runtime-main.00818f46.js"
+  },
+  {
+    "revision": "ade91f473255991f410f61857696434b",
+    "url": "/frontend-applications/static/media/Montserrat-Bold.ade91f47.ttf"
+  },
+  {
+    "revision": "a7063e0c0f0cb546ad45e9e24b27bd3b",
+    "url": "/frontend-applications/static/media/Montserrat-Italic.a7063e0c.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/frontend-applications/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "212329f5b82ea3d36acdc359af1144ce",
+    "url": "/frontend-applications/static/media/logo.212329f5.svg"
+  }
+]);
